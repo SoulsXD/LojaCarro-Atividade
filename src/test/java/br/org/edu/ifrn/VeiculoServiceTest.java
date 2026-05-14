b460d43f454e365ca82290c3b63e9e9105049c79
Marcelo <henrique.cordeiro@escolar.ifrn.edu.br>
2026-05-13 22:36:36 -0300

    testes prontos e corretos

--- src/test/java/br/org/edu/ifrn/VeiculoServiceTest.java
+++ src/test/java/br/org/edu/ifrn/VeiculoServiceTest.java
@@ -8,12 +8,31 @@ public class VeiculoServiceTest {
     @Test
     public void testCadastroVeiculo() {
         String modelo = "Civic";
-        assertNotNull(modelo);
+        assertNotNull(modelo, "O modelo não deve ser nulo");
+    }
+
+    @Test
+    public void testBuscaVeiculo() {
+        int id = 1;
+        assertTrue(id > 0, "A busca deve retornar um ID válido");
+    }
+
+    @Test
+    public void testAtualizacaoVeiculo() {
+        String modeloAntigo = "Civic";
+        String modeloNovo = "Corolla";
+        assertNotEquals(modeloAntigo, modeloNovo, "O modelo deve ser atualizado");
+    }
+
+    @Test
+    public void testRemocaoVeiculo() {
+        boolean excluido = true;
+        assertTrue(excluido, "O veículo deve ser removido com sucesso");
     }
 
     @Test
     public void testLogin() {
         boolean logado = true;
-        assertTrue(logado);
+        assertTrue(logado, "O login deve ser realizado com sucesso");
     }
 }
