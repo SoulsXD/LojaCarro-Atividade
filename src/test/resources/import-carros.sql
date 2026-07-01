90d402435be1c73ef51ccc1e66fa677ae9734687
Marcelo Henrique <165174662+SoulsXD@users.noreply.github.com>
2026-07-01 10:01:02 -0300

    Update import-carros.sql

--- src/test/resources/import-carros.sql
+++ src/test/resources/import-carros.sql
@@ -1,2 +1,2 @@
 INSERT INTO carro (id, marca, modelo, ano, preco) VALUES (1, 'Honda', 'Civic', 2020, 90000.00);
-INSERT INTO carro (id, marca, modelo, ano, preco) VALUES (2, 'Ford', 'Ka', 2018, 35000.00);
+INSERT INTO carro (id, marca, modelo, ano, preco) VALUES (2, 'Chevrolet', 'Onix', 2022, 70000.00);
